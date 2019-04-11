726249f61e485c3ba1b8ffb5a2e8be45fe71bd00
SILPAMANOJ <silpapalassery@gmail.com>
2019-04-11 11:22:48 +0530

    UI till image uploading

--- PicMath/imgcalculator/app1/views.py
+++ PicMath/imgcalculator/app1/views.py
@@ -9,5 +9,16 @@ from django.core.files.storage import FileSystemStorage
 
 def index(request):
     return render(request,"index.html",{})
+    
 def mainpage(request):
     return render(request,"mainpage.html",{})
+
+def upload(request):    
+    if request.method=='POST':
+        myfile=request.FILES['up']
+        fs=FileSystemStorage()
+        filename=fs.save("/home/silpa/Desktop/PicMath/imgcalculator/app1/static/userpic/"+myfile.name,myfile)
+        print(filename)
+        return HttpResponseRedirect("http://127.0.0.1:8000/upload")
+    #return render(request,"upload.html",{})
+    return render(request,"upload.html",{})
